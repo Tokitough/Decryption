ba6925506904544312c1f47008e45e8bd282a6e3
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-04-21 13:30:46 +0800

    Ask user for input

---
+++ Decryption.py
@@ -0,0 +1,12 @@
+#ask user for input
+print("\u001b[30m=" * 70)
+input = input("\u001b[32mInput a string to decrypt: ")
+output = ""
+
+# check characters
+# change '*' = a
+# change '&' = e
+# change '#' = i
+# change '+' = o 
+# change '!' = u
+# print output
