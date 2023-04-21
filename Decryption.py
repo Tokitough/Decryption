c9292e40bcaef37960325d81409e9b87a2300d25
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-04-21 13:40:17 +0800

    Vowel substitution

--- Decryption.py
+++ Decryption.py
@@ -4,9 +4,23 @@ input = input("\u001b[32mInput a string to decrypt: ")
 output = ""
 
 # check characters
+for i in range(len(input)):
 # change '*' = a
+    if input[i] == "*":
+        output += "a"
 # change '&' = e
+    elif input[i] == "&":
+        output += "e"
 # change '#' = i
+    elif input[i] == "#":
+        output += "i"
 # change '+' = o 
+    elif input[i] == "+":
+        output += "o"
 # change '!' = u
+    elif input[i] == "!":
+        output += "u"
+    else:
+        output += input[i]
+        
 # print output
